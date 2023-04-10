bd99eee8ab3383b779f75a88caf5ff7d6fbfd320
“faiz-appinventiv” <mohammad.faiz@appinventiv.com>
2023-04-10 12:23:38 +0530

    code cleaning

--- src/utils/commonFunctions/Encryption.ts
+++ src/utils/commonFunctions/Encryption.ts
@@ -1,25 +1,6 @@
 import crypto from 'crypto';
 import {Config} from '.';
 
-// const iv = crypto.randomBytes(16);
-
-// function encrypt(text: string) {
-//   console.log(
-//     'Encryptioniv',
-//     iv.toString('hex'),
-//     Config.ENCRYPTION_ALGORITHM,
-//     Config.ENCRYPTION_KEY,
-//   );
-//   const cipher = crypto.createCipheriv(
-//     Config.ENCRYPTION_ALGORITHM,
-//     Config.ENCRYPTION_KEY,
-//     iv,
-//   );
-//   let encrypted = cipher.update(text, 'utf-8', 'hex');
-//   encrypted += cipher.final('hex');
-//   return encrypted;
-// }
-
 function encrypt(text: any) {
   const iv = crypto.randomBytes(16);
   const cipher = crypto.createCipheriv(
@@ -45,23 +26,6 @@ function decrypt(data: any) {
   decrypted += decipher.final('utf-8');
   return decrypted;
 }
-
-// function decrypt(text: any) {
-//   console.log(
-//     'Decryptioniv',
-//     iv.toString('hex'),
-//     Config.ENCRYPTION_ALGORITHM,
-//     Config.ENCRYPTION_KEY,
-//   );
-//   const decipher = crypto.createDecipheriv(
-//     Config.ENCRYPTION_ALGORITHM,
-//     Config.ENCRYPTION_KEY,
-//     iv,
-//   );
-//   let decrypted = decipher.update(text, 'hex', 'utf-8');
-//   decrypted += decipher.final('utf-8');
-//   return decrypted;
-// }
 
 export default {
   Encrypt: encrypt,
